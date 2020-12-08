6256ef8dc93720432cd99748d95d382eab4296d8
Yevhen <buhaienkoyevhen@gmail.com>
2020-12-08 18:46:07 +0200

    Добавьте файлы проекта.

---
+++ Classes/MyCar.cs
@@ -0,0 +1,111 @@
+﻿
+
+namespace Classes
+{
+    public partial class MyCar
+    {
+        public static MyCar[] myCarList;
+
+
+        private readonly decimal maxFuel;
+        private readonly int maxSpeed;
+        private bool brakeLight;
+        private decimal fuel;
+        private int speed;
+        private decimal fuelPercent;
+        public decimal Fuel { get { return fuel; } }
+        public int Speed { get { return speed; } }
+        public bool BrakeLight { get { return brakeLight; } private set { brakeLight = false; } }
+        public decimal FuelPercent { get { return (fuel / maxFuel) * 100; } }
+
+        public string infoMaxFuelMaxSpeed;
+        public string infoWarning;
+        public string InfoMaxFuelMaxSpeed
+        {
+            get { return "maxFuel=" + maxFuel + "\tmaxSpeed" + maxSpeed; ; }
+            
+        }
+        public string WarningFuel
+        {
+            get { return fuel < 5 ? "low fuel" : "Fuel "+ FuelPercent;  }
+            
+        }
+
+        static MyCar()
+        {
+            myCarList = new MyCar[0];
+        }
+
+        public MyCar()
+        {
+            this.maxSpeed = 60;
+            this.maxFuel = 10;
+            addToMyCarList();
+        }
+        public MyCar(int maxSpeed, decimal maxFuel)
+        {
+            this.maxSpeed = maxSpeed;
+            this.maxFuel = maxFuel;
+            addToMyCarList();
+        }
+
+        public MyCar(int maxSpeed, decimal maxFuel, decimal fuel)
+        {
+            this.maxSpeed = maxSpeed;
+            this.maxFuel = maxFuel;
+            this.fuel = fuel;
+            addToMyCarList();
+        }
+
+        private void addToMyCarList()
+        {
+            MyCar[] tempList = new MyCar[myCarList.Length + 1];
+            myCarList.CopyTo(tempList, 0);
+            myCarList = tempList;
+            myCarList[myCarList.Length - 1] = this;
+        }
+
+        public void movement()
+        {
+            brakeLight = false;
+            if (fuel <= 0 & speed > 0)
+            {
+                speed--;
+                return;
+            }
+            if (speed <= maxSpeed & fuel > 0)
+            {
+                speed++;
+                fuel--;
+            }
+        }
+
+        public void brakeCar()
+        {
+            brakeLight = true;
+            if (speed == 0) return;
+            else speed--;
+        }
+
+        public void addFuel()
+        {
+            if (speed == 0 & fuel < maxFuel / 2)
+            {
+                while (fuel != maxFuel)
+                {
+                    fuel++;
+                }
+            }
+            else return;// throw new Exception(" Бак заполнен! ");
+        }
+        public string myCarInfo()
+        {
+            string stringMyCar =
+                (brakeLight ? " brakeLight \n" : "\n") +
+                maxFuel + "  fuel " + fuel + "\n" +
+                maxSpeed + " speed " + speed + "\n" +
+                 "fuelPercent " + FuelPercent + "\n";
+            return stringMyCar;
+        }
+    }
+}
---
+++ Classes/MyCar1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Classes
+{
+    public partial class MyCar
+    {
+        public void beep()
+        {
+            Console.WriteLine(" BEEP ");
+            
+            Console.Beep(2500, 100);
+        }
+
+        public void speedGraikInfo()
+        {
+            for (int i = 0; i < speed; i++)
+            {
+
+                Console.Write("-");
+            }
+            // Console.Beep(50 * (speed+1) , 500);
+            Console.Write("> "+speed);
+        }
+    }
+}
